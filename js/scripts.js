beee6f91e1e972679e3a8735266ec99a0e72fa60
Ula Chin <ula.chin092@gmail.com>
2021-11-22 13:56:22 -0500

    html

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );    
+    $("#carouselButton").on("click",function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+    $(function(){
+        $('#loginButton').on("click", function(){
+            $('#loginModal').modal('show');
+        });
+        $('#reserveButton').on("click",function(){
+            $('#reserveModal').modal('show');
+        });
+        });
+  });
